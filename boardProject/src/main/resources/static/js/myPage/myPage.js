84317e438cd8d432d32402ecd74e25dd5ca1b45b
DESKTOP-QDV2AJ2\Galaxy Book Pro <027620@naver.com>
2024-04-03 14:06:19 +0900

    마이페이지 만들기

---
+++ boardProject/src/main/resources/static/js/myPage/myPage.js
@@ -0,0 +1,10 @@
+// 마이페이지 전부이 지금 js를 다 쓸거다
+// 그러므로 오류를 방지하는 방식으로 코드 작성
+
+/* 회원정보 수정 페이지 */
+const updateInfo = document.querySelector("#updateInfo"); // form 태그
+
+// #updateInfo 요소가 존재할때만 수행
+if(updateInfo != null){
+
+}
